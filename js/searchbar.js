4014e5af1bcc4bfeb0aa52b8da5a448ae0c3ff24
Tyler Pfledderer <themindfultrumpet@gmail.com>
2020-06-15 22:31:06 -0400

    Update search field to "reset" when the X is clicked, using "onsearch" attribute.

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,22 +1,29 @@
 function search_bar() {
+  // Get user input in search field and set text to lowercase.
 	let input = document.getElementById('search-bar').value;
   input = input.toLowerCase();
-  console.log(input);
+
+  // Grab array of paragraph tags that contain caption text
   let caption = document.getElementById('captionGroup');
   let capText = caption.querySelectorAll('.caption > p');
 
+  // Grab array of the anchor tags containing the photo thumbnails
   let image = document.querySelectorAll('.gallery__link');
   
+  // Loop through each <p> containing the caption text.
   for (let i = 0; i < capText.length; i++) {
+    // Grab caption text in each p tag, set to lowercase, and check if the user input is anywhere in the string.
     let search = capText[i]
       .textContent
       .toLowerCase()
       .includes(input);
-    if (!search || !input === null) {
+
+    // If the user input is not in the caption text, hide the image. Else, make sure it's still displayed.
+    if (!search) {
       image[i].style.display = "none";
     } else {
       image[i].style.display = "inline-block";
     }
-    
   }
 }
+
